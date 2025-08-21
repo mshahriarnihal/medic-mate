6b178f6de89f63e23e8957865022c907f58b18c5
Mubasshir Al Shahriar <shahriar.nihal@yahoo.com>
2025-08-21 00:53:06 -0400

    page.js update 3.3.1

--- src/app/meds/page.js
+++ src/app/meds/page.js
@@ -1,7 +1,7 @@
 'use client';
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
-
+ 
 
 /* adding this part for the CapsuleButton */
 function CapsuleButton({ taken, required, onMark }) {
